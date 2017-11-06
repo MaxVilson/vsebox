5d52643882f1d49dec7e1cd7442fcdb136469b63
Max <maxim00066@gmail.com>
2017-11-06 20:54:58 +0300

    commit

---
+++ src/blocks/roulette/roulette.js
@@ -0,0 +1,5 @@
+var rouletteItem = document.querySelectorAll('.item');
+var randomNumber = Math.random(0, 10);
+for (var i = 0; i < rouletteItem.length; i++) {
+  rouletteItem[i].style.transform = 'translateX(-1800px)';
+}
