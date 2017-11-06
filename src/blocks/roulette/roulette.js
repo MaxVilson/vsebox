75f80a87f45261fc18593f68dedc32ffd528bbf7
Max <maxim00066@gmail.com>
2017-11-06 22:59:19 +0300

    commit

--- src/blocks/roulette/roulette.js
+++ src/blocks/roulette/roulette.js
@@ -1,5 +1,13 @@
+var btnRoulette = document.getElementById('btn-roulette');
 var rouletteItem = document.querySelectorAll('.item');
-var randomNumber = Math.random(0, 10);
-for (var i = 0; i < rouletteItem.length; i++) {
-  rouletteItem[i].style.transform = 'translateX(-2100px)';
+
+btnRoulette.onclick = function() {
+  for (var i = 0; i < rouletteItem.length; i++) {
+    rouletteItem[i].style.transform = 'translateX(-2100px)';
+  }
+  setTimeout(modalShow, 3500);
+}
+
+function modalShow() {
+  $("#modal-demo-04").modal('show');
 }
