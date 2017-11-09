b58ac08b8aa8c14f0859e8cf8ccca5201280280b
Max <maxim00066@gmail.com>
2017-11-09 12:53:49 +0300

    commit

--- src/blocks/roulette/roulette.js
+++ src/blocks/roulette/roulette.js
@@ -1,13 +1,25 @@
 var btnRoulette = document.getElementById('btn-roulette');
+var btnOpen = document.getElementById('openBox');
 var rouletteItem = document.querySelectorAll('.item');
+var rouletteCount = getRandomInt(0, 30) * 150;
 
-btnRoulette.onclick = function() {
+btnOpen.onclick = function() {
+  $("#modal-demo-04").modal('hide');
+}
+
+btnRoulette.addEventListener('click', scrolling);
+
+function scrolling() {
   for (var i = 0; i < rouletteItem.length; i++) {
-    rouletteItem[i].style.transform = 'translateX(-2100px)';
+    rouletteItem[i].style.transform = 'translateX('+-rouletteCount+'px)';
   }
   setTimeout(modalShow, 3500);
 }
 
 function modalShow() {
   $("#modal-demo-04").modal('show');
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
